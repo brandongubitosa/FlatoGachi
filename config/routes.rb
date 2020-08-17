4d2865ba0af80239317d386d5bac0fcd856a0723
Luis Reyes <luisreyesxv@yahoo.com>
2020-08-17 10:30:38 -0400

    making changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :names
+  resources :user_monsters
   resources :monsters
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
